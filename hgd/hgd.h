a1508de6f8d93ef0535c1143feadd185de26893a
vext01 <vext01@gmail.com>
2011-01-02 02:37:51 +0000

    import

---
+++ hgd/hgd.h
@@ -0,0 +1,24 @@
+#ifndef __HGD_H
+#define __HGD_H
+
+#define HGD_DFL_DB_PATH		"/home/edd/hgd.db"
+#define HGD_DFL_MPLAYER_PID_PATH "/home/edd/hgd_mplayer.pid"
+
+struct hgd_playlist_item {
+	int			id;
+	char			*filename;
+	char			*user;
+	uint8_t			playing;
+	uint8_t			finished;
+};
+
+/* simple debug facility */
+#define DPRINTF(x...)           do { if (hgd_debug)		\
+					    fprintf(stderr, x); } while (0)
+void				hgd_free_playlist_item(
+				    struct hgd_playlist_item *);
+
+void				*xmalloc(size_t);
+int				xasprintf(char **buf, char *fmt, ...);
+
+#endif
