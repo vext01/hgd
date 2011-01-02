8556a6d16f507204675276ea44f93bb36e6c4af4
vext01 <vext01@gmail.com>
2011-01-02 13:37:06 +0000

    scaffold hgd-netd

---
+++ hgd/hgd-netd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int
+main(void)
+{
+	return (EXIT_SUCCESS);
+}
