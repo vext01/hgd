2544a92f205b3369b0c391064279de2b1ad46dca
vext01 <vext01@gmail.com>
2011-01-04 17:56:01 +0000

    add some higher level socket operations.

--- hgd/hgd.h
+++ hgd/hgd.h
@@ -7,11 +7,11 @@
 #include <stdint.h>
 
 struct hgd_playlist_item {
-	int			id;
+	int			 id;
 	char			*filename;
 	char			*user;
-	uint8_t			playing;
-	uint8_t			finished;
+	uint8_t			 playing;
+	uint8_t			 finished;
 };
 
 /* simple debug facility */
@@ -21,6 +21,10 @@ void				hgd_free_playlist_item(
 				    struct hgd_playlist_item *);
 
 void				*xmalloc(size_t);
-int				xasprintf(char **buf, char *fmt, ...);
+int				 xasprintf(char **buf, char *fmt, ...);
+void				 hgd_sock_send(int fd, char *msg);
+void				 hgd_sock_send_line(int fd, char *msg);
+char				*hgd_sock_recv(int fd, ssize_t len);
+char				*hgd_sock_recv_line(int fd);
 
 #endif
