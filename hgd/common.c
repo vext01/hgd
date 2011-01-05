94a313a39da89f5568de30d16bfd9f069f1ee4a1
vext01 <vext01@gmail.com>
2011-01-05 20:40:16 +0000

    scaffold client, move db functions elsewhere, as client does not need them

--- hgd/common.c
+++ hgd/common.c
@@ -154,38 +154,3 @@ hgd_sock_recv_line(int fd)
 
 	return full_msg;
 }
-
-/* Open, create and initialise database */
-sqlite3 *
-hgd_open_db(char *db_path)
-{
-	int			sql_res;
-	char			*sql_err;
-	sqlite3			*db;
-
-	/* open the database */
-	if (sqlite3_open(db_path, &db)) {
-		fprintf(stderr, "%s: can't open db: %s\n",
-		    __func__, sqlite3_errmsg(db));
-		return NULL;
-	}
-
-	sql_res = sqlite3_exec(db,
-	    "CREATE TABLE IF NOT EXISTS playlist ("
-	    "id INTEGER PRIMARY KEY,"
-	    "filename VARCHAR(50),"
-	    "user VARCHAR(15),"
-	    "playing INTEGER,"
-	    "finished INTEGER)",
-	    NULL, NULL, &sql_err);
-
-	if (sql_res != SQLITE_OK) {
-		fprintf(stderr, "%s: can't initialise db: %s\n",
-		    __func__, sqlite3_errmsg(db));
-		sqlite3_close(db);
-		return NULL;
-	}
-	DPRINTF("%s: database open\n", __func__);
-	return db;
-}
-
