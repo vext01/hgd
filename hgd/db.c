22772e075b69092837ecafdb4763e1aa6ef38b36
vext01 <vext01@gmail.com>
2011-01-16 21:41:03 +0000

    make vote table structure when creating tables. make username and filename lengths for database macros.

--- hgd/db.c
+++ hgd/db.c
@@ -21,17 +21,19 @@ hgd_open_db(char *db_path)
 	sqlite3			*db;
 
 	/* open the database */
+	DPRINTF("%s: opening database\n", __func__);
 	if (sqlite3_open(db_path, &db)) {
 		fprintf(stderr, "%s: can't open db: %s\n",
 		    __func__, sqlite3_errmsg(db));
 		return NULL;
 	}
 
+	DPRINTF("%s: making playlist table (if needed)\n", __func__);
 	sql_res = sqlite3_exec(db,
 	    "CREATE TABLE IF NOT EXISTS playlist ("
 	    "id INTEGER PRIMARY KEY,"
-	    "filename VARCHAR(50),"
-	    "user VARCHAR(15),"
+	    "filename VARCHAR(" HGD_DBS_FILENAME_LEN " ),"
+	    "user VARCHAR(" HGD_DBS_USERNAME_LEN "),"
 	    "playing INTEGER,"
 	    "finished INTEGER)",
 	    NULL, NULL, &sql_err);
@@ -40,9 +42,28 @@ hgd_open_db(char *db_path)
 		fprintf(stderr, "%s: can't initialise db: %s\n",
 		    __func__, sqlite3_errmsg(db));
 		sqlite3_close(db);
+		sqlite3_free(sql_err);
 		return NULL;
 	}
-	DPRINTF("%s: database open\n", __func__);
+
+	DPRINTF("%s: making votes table (if needed)\n", __func__);
+	sql_res = sqlite3_exec(db,
+	    "CREATE TABLE IF NOT EXISTS votes ("
+	    "user VARCHAR(" HGD_DBS_USERNAME_LEN "),"
+	    "track_id INTEGER,"
+	    " PRIMARY KEY (user, track_id))",
+	    NULL, NULL, &sql_err);
+
+	if (sql_res != SQLITE_OK) {
+		fprintf(stderr, "%s: can't initialise db: %s\n",
+		    __func__, sqlite3_errmsg(db));
+		sqlite3_close(db);
+		sqlite3_free(sql_err);
+		return NULL;
+	}
+
+	DPRINTF("%s: database open and ready\n", __func__);
+
 	return db;
 }
 
