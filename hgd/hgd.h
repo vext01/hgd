05eff9c2cfefdf91caee774e5dfaa36adaf01060
edd <edd>
2011-01-22 22:00:35 +0000

    oops

--- hgd/hgd.h
+++ hgd/hgd.h
@@ -31,9 +31,9 @@
 
 /* networking */
 #define HGD_DFL_PORT		6633
-#define HGD_DFL_BACKLOG		128
+#define HGD_DFL_BACKLOG		10
 #define HGD_DFL_MAX_UPLOAD	(1024 * 1024 * 50)
-#define HGD_MAX_LINE		5
+#define HGD_MAX_LINE		128
 #define HGD_BINARY_CHUNK	4096
 
 /* database schema */
