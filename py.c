c19e8d5eda03cde30533bdc98da552596066fd52
vext01 <vext01@gmail.com>
2011-05-21 16:19:07 +0100

    show how to implement a method in C exposed as python

--- py.c
+++ py.c
@@ -31,44 +31,101 @@
 struct hgd_py_modules		hgd_py_mods;
 
 /*
- * Python extension glue
- *
  * XXX - reference counts, all over the place!
  */
 
+/*
+ * methods exposed to python
+ */
+
+/*
+ * Testing a method call
+ * args :
+ * returns : None
+ */
+static PyObject *
+hgd_py_meth_test(Hgd *self)
+{
+	self = self; /* sssh */
+	printf("Ya, hi, i work\n");
+	Py_RETURN_NONE;
+}
+
+/* method table */
+static PyMethodDef hgd_methods[] = {
+	{"test",
+	    (PyCFunction) hgd_py_meth_test, METH_NOARGS, "test the damned thing"},
+	{ 0, 0, 0, 0 }
+};
+
+/*
+ * Describe the hgd object type
+ * This is for Python 2.6 and will probably make warnings on other versions
+ */
 static PyTypeObject HgdType = {
 	PyObject_HEAD_INIT(NULL)
-	0,				/*ob_size*/
-	"hgd.Hgd",			/*tp_name*/
-	sizeof(Hgd),			/*tp_basicsize*/
-	0,				/*tp_itemsize*/
-	0,				/*tp_dealloc*/
-	0,				/*tp_print*/
-	0,				/*tp_getattr*/
-	0,				/*tp_setattr*/
-	0,				/*tp_compare*/
-	0,				/*tp_repr*/
-	0,				/*tp_as_number*/
-	0,				/*tp_as_sequence*/
-	0,				/*tp_as_mapping*/
-	0,				/*tp_hash*/
-	0,				/*tp_call*/
-	0,				/*tp_str*/
-	0,				/*tp_getattro*/
-	0,				/*tp_setattro*/
-	0,				/*tp_as_buffer*/
-	Py_TPFLAGS_DEFAULT,		/*tp_flags*/
-	"Hackathon Gunther Daemon",	/*tp_doc*/
+	0,				/* ob_size */
+	"hgd.Hgd",			/* tp_name */
+	sizeof(Hgd),			/* tp_basicsize */
+	0,				/* tp_itemsize */
+	0,				/* tp_dealloc */
+	0,				/* tp_print */
+	0,				/* tp_getattr */
+	0,				/* tp_setattr */
+	0,				/* tp_compare */
+	0,				/* tp_repr */
+	0,				/* tp_as_number */
+	0,				/* tp_as_sequence */
+	0,				/* tp_as_mapping */
+	0,				/* tp_hash */
+	0,				/* tp_call */
+	0,				/* tp_str */
+	0,				/* tp_getattro */
+	0,				/* tp_setattro */
+	0,				/* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,		/* tp_flags */
+	"Hackathon Gunther Daemon",	/* tp_doc */
+	0,				/* tp_traverse */
+	0,				/* tp_clear */
+	0,				/* tp_richcompare */
+	0,				/* tp_weaklistoffset */
+	0,				/* tp_iter */
+	0,				/* tp_iternext */
+	hgd_methods,			/* tp_methods */
+	0,				/* tp_members */
+	0,				/* tp_getset */
+	0,				/* tp_base */
+	0,				/* tp_dict */
+	0,				/* tp_descr_get */
+	0,				/* tp_descr_set */
+	0,				/* tp_dictoffset */
+	0,				/* tp_init */
+	0,				/* tp_alloc */
+	0,				/* tp_new */
+	0,				/* tp_free */
+	0,				/* tp_is_gc */
+	0,				/* tp_bases */
+	0,				/* tp_mro */
+	0,				/* tp_cache */
+	0,				/* tp_subclasses */
+	0,				/* tp_weaklis */
+	0,				/* destructor */
+	0,				/* tp_version_tag */
+#ifdef COUNT_ALLOCS
+	0,				/* tp_allocs */
+	0,				/* tp_frees */
+	0,				/* tp_maxalloc */
+	0,				/* tp_prev */
+	0,				/* tp_next */
+#endif
 };
 
-static PyMethodDef hgd_methods[] = {
-    {NULL}
-};
-
-#ifndef PyMODINIT_FUNC	/* declarations for DLL import/export */
+/*
+ * initialise hgd module
+ */
+#ifndef PyMODINIT_FUN
 #define PyMODINIT_FUNC void
 #endif
-
 PyMODINIT_FUNC
 inithgd(void)
 {
