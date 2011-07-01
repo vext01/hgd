97ea75c19bc66098cac2440bd8f67a789a1a5071
vext01 <vext01@gmail.com>
2011-07-02 00:52:56 +0100

    worked out how to daemonise

--- hgd-netd.c
+++ hgd-netd.c
@@ -41,6 +41,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <poll.h>
+#include <sys/wait.h>
 
 #include "hgd.h"
 #include "db.h"
@@ -1207,7 +1208,8 @@ main(int argc, char **argv)
 {
 	char			 ch, *xdg_config_home;
 	char			*config_path[4] = {NULL, NULL, NULL, NULL};
-	int			 num_config = 2;
+	int			 num_config = 2, fg_status;
+	pid_t			 bg_pid;
 
 	/* as early as possible */
 	HGD_INIT_SYSLOG_DAEMON();
@@ -1383,6 +1385,20 @@ main(int argc, char **argv)
 		hgd_exit_nicely();
 	}
 
+	/* alright, everything looks good, lets be a daemon and background */
+	/* XXX make re-usable */
+	bg_pid = fork();
+	if (bg_pid) {
+		DPRINTF(HGD_D_INFO, "Backgrounding. PID=%d", bg_pid);
+		exit_ok = 1;
+		hgd_exit_nicely();
+	} else {
+		/* child */
+		if (setsid() != getpid()) {
+			DPRINTF(HGD_D_ERROR, "failed to setsid: %s", SERROR);
+			hgd_exit_nicely();
+		}
+	}
 	hgd_listen_loop();
 
 	exit_ok = 1;
